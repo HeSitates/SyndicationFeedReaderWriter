e09bef66d56e0afe621a3a65a1959ba83b853aa2
Henk Simmelink <henk@sisode.nl>
2024-01-01 16:56:35 +0100

    Set tab to two spaces.

--- examples/CreateSimpleRssFeedExample.cs
+++ examples/CreateSimpleRssFeedExample.cs
@@ -15,76 +15,76 @@ using Microsoft.SyndicationFeed.Rss;
 /// </summary>
 class CreateSimpleRssFeed
 {
-    public static async Task WriteFeed()
-    {
-        var sw = new StringWriterWithEncoding(Encoding.UTF8);
+  public static async Task WriteFeed()
+  {
+    var sw = new StringWriterWithEncoding(Encoding.UTF8);
 
-        using (var xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings() { Async = true , Indent = true }))
+    using (var xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings() { Async = true, Indent = true }))
+    {
+      var writer = new RssFeedWriter(xmlWriter);
+
+      //
+      // Add Title
+      await writer.WriteTitle("Example of RssFeedWriter");
+
+      //
+      // Add Description
+      await writer.WriteDescription("Hello World, RSS 2.0!");
+
+      //
+      // Add Link
+      await writer.Write(new SyndicationLink(new Uri("https://github.com/dotnet/SyndicationFeedReaderWriter")));
+
+      //
+      // Add managing editor
+      await writer.Write(new SyndicationPerson("managingeditor", "managingeditor@contoso.com", RssContributorTypes.ManagingEditor));
+
+      //
+      // Add publish date
+      await writer.WritePubDate(DateTimeOffset.UtcNow);
+
+      //
+      // Add custom element
+      var customElement = new SyndicationContent("customElement");
+
+      customElement.AddAttribute(new SyndicationAttribute("attr1", "true"));
+      customElement.AddField(new SyndicationContent("Company", "Contoso"));
+
+      await writer.Write(customElement);
+
+      //
+      // Add Items
+      for (int i = 0; i < 5; ++i)
+      {
+        var item = new SyndicationItem()
         {
-            var writer = new RssFeedWriter(xmlWriter);
+          Id = "https://www.nuget.org/packages/Microsoft.SyndicationFeed.ReaderWriter",
+          Title = $"Item #{i + 1}",
+          Description = "The new Microsoft.SyndicationFeed.ReaderWriter is now available as a NuGet package!",
+          Published = DateTimeOffset.UtcNow
+        };
 
-            //
-            // Add Title
-            await writer.WriteTitle("Example of RssFeedWriter");
+        item.AddLink(new SyndicationLink(new Uri("https://github.com/dotnet/SyndicationFeedReaderWriter")));
+        item.AddCategory(new SyndicationCategory("Technology"));
+        item.AddContributor(new SyndicationPerson("user", "user@contoso.com"));
 
-            //
-            // Add Description
-            await writer.WriteDescription("Hello World, RSS 2.0!");
+        await writer.Write(item);
+      }
 
-            //
-            // Add Link
-            await writer.Write(new SyndicationLink(new Uri("https://github.com/dotnet/SyndicationFeedReaderWriter")));
-
-            //
-            // Add managing editor
-            await writer.Write(new SyndicationPerson("managingeditor", "managingeditor@contoso.com", RssContributorTypes.ManagingEditor));
-
-            //
-            // Add publish date
-            await writer.WritePubDate(DateTimeOffset.UtcNow);
-
-            //
-            // Add custom element
-            var customElement = new SyndicationContent("customElement");
-
-            customElement.AddAttribute(new SyndicationAttribute("attr1", "true"));
-            customElement.AddField(new SyndicationContent("Company", "Contoso"));
-
-            await writer.Write(customElement);
-
-            //
-            // Add Items
-            for (int i = 0; i < 5; ++i)
-            {
-                var item = new SyndicationItem()
-                {
-                    Id = "https://www.nuget.org/packages/Microsoft.SyndicationFeed.ReaderWriter",
-                    Title = $"Item #{i + 1}",
-                    Description = "The new Microsoft.SyndicationFeed.ReaderWriter is now available as a NuGet package!",
-                    Published = DateTimeOffset.UtcNow
-                };
-
-                item.AddLink(new SyndicationLink(new Uri("https://github.com/dotnet/SyndicationFeedReaderWriter")));
-                item.AddCategory(new SyndicationCategory("Technology"));
-                item.AddContributor(new SyndicationPerson("user", "user@contoso.com"));
-
-                await writer.Write(item);
-            }
-
-            //
-            // Done
-            await xmlWriter.FlushAsync();
-        }
-
-        //
-        // Ouput the feed
-        Console.WriteLine(sw.ToString());
+      //
+      // Done
+      await xmlWriter.FlushAsync();
     }
 
-    private class StringWriterWithEncoding : StringWriter
-    {
-      public StringWriterWithEncoding(Encoding encoding) => Encoding = encoding;
+    //
+    // Ouput the feed
+    Console.WriteLine(sw.ToString());
+  }
 
-        public override Encoding Encoding { get; }
-    }
+  private class StringWriterWithEncoding : StringWriter
+  {
+    public StringWriterWithEncoding(Encoding encoding) => Encoding = encoding;
+
+    public override Encoding Encoding { get; }
+  }
 }
--- examples/RssWriteItemWithCustomElementExample.cs
+++ examples/RssWriteItemWithCustomElementExample.cs
@@ -17,61 +17,62 @@ using Microsoft.SyndicationFeed.Rss;
 /// </summary>
 class RssWriteItemWithCustomElement
 {
-    public static async Task WriteCustomItem()
-    {
-        const string ExampleNs = "http://contoso.com/syndication/feed/examples";
-        var sw = new StringWriterWithEncoding(Encoding.UTF8);
+  public static async Task WriteCustomItem()
+  {
+    const string ExampleNs = "http://contoso.com/syndication/feed/examples";
+    var sw = new StringWriterWithEncoding(Encoding.UTF8);
 
-        using (var xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings() { Async = true, Indent = true }))
-        {
-            var attributes = new List<SyndicationAttribute>()
+    using (var xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings() { Async = true, Indent = true }))
+    {
+      var attributes = new List<SyndicationAttribute>()
             {
                 new SyndicationAttribute("xmlns:example", ExampleNs)
             };
 
-            var formatter = new RssFormatter(attributes, xmlWriter.Settings);
-            var writer = new RssFeedWriter(xmlWriter, attributes, formatter);
-              
-            // Create item
-            var item = new SyndicationItem()
-            {
-                Title = "Rss Writer Available",
-                Description = "The new RSS Writer is now available as a NuGet package!",
-                Id = "https://www.nuget.org/packages/Microsoft.SyndicationFeed",
-                Published = DateTimeOffset.UtcNow
-            };
+      var formatter = new RssFormatter(attributes, xmlWriter.Settings);
+      var writer = new RssFeedWriter(xmlWriter, attributes, formatter);
 
-            item.AddCategory(new SyndicationCategory("Technology"));
-            item.AddContributor(new SyndicationPerson("test", "test@mail.com"));
+      // Create item
+      var item = new SyndicationItem()
+      {
+        Title = "Rss Writer Available",
+        Description = "The new RSS Writer is now available as a NuGet package!",
+        Id = "https://www.nuget.org/packages/Microsoft.SyndicationFeed",
+        Published = DateTimeOffset.UtcNow
+      };
 
-            //
-            // Format the item as SyndicationContent
-            var content = new SyndicationContent(formatter.CreateContent(item));
+      item.AddCategory(new SyndicationCategory("Technology"));
+      item.AddContributor(new SyndicationPerson("test", "test@mail.com"));
 
-            // Add custom fields/attributes
-            content.AddField(new SyndicationContent("customElement", ExampleNs, "Custom Value"));
+      //
+      // Format the item as SyndicationContent
+      var content = new SyndicationContent(formatter.CreateContent(item));
 
-            // Write 
-            await writer.Write(content);
+      // Add custom fields/attributes
+      content.AddField(new SyndicationContent("customElement", ExampleNs, "Custom Value"));
 
-            // Done
-            await xmlWriter.FlushAsync();
-        }
+      // Write 
+      await writer.Write(content);
 
-        Console.WriteLine(sw.ToString());
+      // Done
+      await xmlWriter.FlushAsync();
     }
-    
-    class StringWriterWithEncoding : StringWriter
+
+    Console.WriteLine(sw.ToString());
+  }
+
+  class StringWriterWithEncoding : StringWriter
+  {
+    private readonly Encoding _encoding;
+
+    public StringWriterWithEncoding(Encoding encoding)
     {
-        private readonly Encoding _encoding;
-
-        public StringWriterWithEncoding(Encoding encoding)
-        {
-            this._encoding = encoding;
-        }
-
-        public override Encoding Encoding {
-            get { return _encoding; }
-        }
+      this._encoding = encoding;
     }
+
+    public override Encoding Encoding
+    {
+      get { return _encoding; }
+    }
+  }
 }
