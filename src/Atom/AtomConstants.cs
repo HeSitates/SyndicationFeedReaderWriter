8a1527c323d78bf82d42d7f14a6707d36ac7fa72
Henk Simmelink <henk@sisode.nl>
2024-01-05 17:59:21 +0100

    Suppress warnings for namespace url's.

--- src/Atom/AtomConstants.cs
+++ src/Atom/AtomConstants.cs
@@ -2,8 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Microsoft.SyndicationFeed.Atom;
 
+[SuppressMessage("Security", "S5332: Using http protocol is insecure", Justification = "Just a namespace here.")]
+[SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Suppressing is ok here.")]
 internal static class AtomConstants
 {
   public const string Atom10Namespace = "http://www.w3.org/2005/Atom";
--- src/Rss/RssConstants.cs
+++ src/Rss/RssConstants.cs
@@ -2,9 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Microsoft.SyndicationFeed.Rss;
 
-static class RssConstants
+[SuppressMessage("Security", "S5332: Using http protocol is insecure", Justification = "Just a namespace here.")]
+[SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Suppressing is ok here.")]
+internal static class RssConstants
 {
   public const string Rss20Namespace = "";
   public const string SpecificationLink = "http://blogs.law.harvard.edu/tech/rss";
